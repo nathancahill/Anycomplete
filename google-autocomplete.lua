2630c4a4ea489f42b4df813d5aa76f71667e8a51
Nathan Cahill <nathan@nathancahill.com>
2016-11-29 13:14:18 -0500

    init

---
+++ google-autocomplete.lua
@@ -0,0 +1,37 @@
+
+local urlencode = require("urlencode")
+
+-- Google Autocomplete
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "G", function()
+    local GOOGLE_ENDPOINT = 'https://www.google.com/complete/search?client=hp&hl=en&xhr=t&q=%s'
+
+    local chooser = hs.chooser.new(function(choosen)
+        hs.window.frontmostWindow():focus()
+        hs.eventtap.keyStrokes(choosen.text)
+    end)
+
+    chooser:queryChangedCallback(function(string)
+        local query = urlencode.string(string)
+
+        hs.http.asyncGet(string.format(GOOGLE_ENDPOINT, query), nil, function(status, data)
+            if not data then return end
+
+            local results = hs.json.decode(data)
+
+            if not results then return end
+
+            choices = hs.fnutils.imap(results[2], function(result)
+                return {
+                    ["text"] = string.gsub(result[1], '</?b>', ''),
+                }
+            end)
+
+            chooser:choices(choices)
+        end)
+    end)
+
+    chooser:rows(5)
+    chooser:searchSubText(false)
+
+    chooser:show()
+end)
