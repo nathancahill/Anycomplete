c9d6f29439483e9c33f234c347773d33989b1269
Nuno Santos <nunofvsantos@gmail.com>
2016-11-30 13:53:30 +0100

    Debounce requests by using a timer, to avoid one request per key stroke.

--- anycomplete.lua
+++ anycomplete.lua
@@ -3,6 +3,7 @@
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "G", function()
     local GOOGLE_ENDPOINT = 'https://suggestqueries.google.com/complete/search?client=firefox&q=%s'
     local current = hs.application.frontmostApplication()
+    local timer;
 
     local chooser = hs.chooser.new(function(choosen)
         current:activate()
@@ -10,21 +11,30 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "G", function()
     end)
 
     chooser:queryChangedCallback(function(string)
-        local query = hs.http.encodeForQuery(string)
+        if timer and timer:running() then
+            timer:stop()
+        end
 
-        hs.http.asyncGet(string.format(GOOGLE_ENDPOINT, query), nil, function(status, data)
-            if not data then return end
+        timer = hs.timer.doAfter(0.3, function()
+            if not string or string == "" then return end
 
-            local ok, results = pcall(function() return hs.json.decode(data) end)
-            if not ok then return end
+            -- hs.alert.show(string)
+            local query = hs.http.encodeForQuery(string)
 
-            choices = hs.fnutils.imap(results[2], function(result)
-                return {
-                    ["text"] = result,
-                }
+            hs.http.asyncGet(string.format(GOOGLE_ENDPOINT, query), nil, function(status, data)
+                if not data then return end
+
+                local ok, results = pcall(function() return hs.json.decode(data) end)
+                if not ok then return end
+
+                choices = hs.fnutils.imap(results[2], function(result)
+                    return {
+                        ["text"] = result,
+                    }
+                end)
+
+                chooser:choices(choices)
             end)
-
-            chooser:choices(choices)
         end)
     end)
 
