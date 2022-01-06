7c5cef50c294d870f7b1964276e3422a05af38f9
Nathan Cahill <nathan@nathancahill.com>
2022-01-06 13:00:16 -0600

    reorganize as Spoon

--- README.md
+++ README.md
@@ -6,36 +6,18 @@ The magic of Google Autocomplete while you're typing. Anywhere.
 
 ### Installation
 
-Anycomplete is an extension for [Hammerspoon](http://hammerspoon.org/). Once Hammerspoon is installed (see [install Hammerspoon](#install-hammerspoon) below) you can run the following script to install Autocomplete.
+Anycomplete is an extension for [Hammerspoon](http://hammerspoon.org/). Once Hammerspoon is installed, you can install the Autocomplete Spoon:
 
-    $ curl -sSL https://raw.githubusercontent.com/nathancahill/Anycomplete/master/install.sh | bash
-
-[install.sh](https://github.com/nathancahill/Anycomplete/blob/master/install.sh) just clones this repository into `~/.hammerspoon`, loads it into Hammerspoon and sets `⌃⌥⌘G` as the default keybinding.
-
-#### Manual installation
-
-    $ git clone https://github.com/nathancahill/anycomplete.git ~/.hammerspoon/anycomplete
+    $ git clone https://github.com/nathancahill/anycomplete.git ~/.hammerspoon/Spoons/Anycomplete.spoon
 
 To initialize, add to `~/.hammerspoon/init.lua` (creating it if it does not exist):
 
-    local anycomplete = require "anycomplete/anycomplete"
-    anycomplete.registerDefaultBindings()
-
-Alternatively, copy `anycomplete.lua` from this repository to wherever
-you keep other Hammerspoon modules and load it appropriately.
+```lua
+anycomplete = hs.loadSpoon("Anycomplete")
+anycomplete.bindHotkeys()
+```
 
 Reload the Hammerspoon config.
-
-#### Install Hammerspoon
-
-Hammerspoon can be installed using [homebrew/caskroom](https://caskroom.github.io/).
-
-    $ brew cask install hammerspoon
-    $ open -a /Applications/Hammerspoon.app
-
-Accessibility must be enabled for Anycomplete to work.
-
-![](https://cloud.githubusercontent.com/assets/220827/20860328/a7dc4344-b975-11e6-893a-bb139ba8a102.png)
 
 ### Usage
 
@@ -43,11 +25,13 @@ Trigger with the hotkey `⌃⌥⌘G`. Once you start typing, suggestions will po
 They can be choosen with `⌘1-9` or by pressing the arrow keys and Enter.
 Pressing `⌘C` copies the selected item to the clipboard.
 
-The hotkey can be changed by passing in arguments to
-`registerDefaultBindings` call (in your `~/.hammerspoon/init.lua` file)
+The hotkey can be changed by passing an argument to
+`bindHotkeys` call (in your `~/.hammerspoon/init.lua` file)
 such as:
 
-    anycomplete.registerDefaultBindings({"cmd", "ctrl"}, 'L')
+```lua
+anycomplete.bindHotkeys({{"cmd", "ctrl"}, "L"})
+```
 
 ### Warning
 
@@ -55,13 +39,11 @@ Google might block your IP address if you use this. See [#26](https://github.com
 
 ### Privacy
 
-No keystrokes are sent to Google until you trigger the hotkey and start typing. If you prefer DuckDuckGo, replace `GOOGLE_ENDPOINT` with:
-`'https://duckduckgo.com/ac/?q=%s'` and the `imap` function with this:
+No keystrokes are sent to Google until you trigger the hotkey and start typing. If you prefer DuckDuckGo,
+set the `engine` option:
 
-```
-choices = hs.fnutils.imap(results, function(result)
-    return {
-        ["text"] = result["phrase"],
-    }
-end)
+```lua
+anycomplete = hs.loadSpoon("Anycomplete")
+anycomplete.engine = "duckduckgo"
+anycomplete.bindHotkeys()
 ```
--- init.lua
+++ init.lua
@@ -1,2 +1,110 @@
-local anycomplete = require "anycomplete/anycomplete"
-anycomplete.registerDefaultBindings()
+--- === Anycomplete ===
+---
+--- The magic of Google Autocomplete while you're typing. Anywhere.
+---
+--- Configurable properties (with default values):
+---     engine = "google", -- or "duckduckgo"
+---
+--- Download: [https://github.com/nathancahill/Anycomplete](https://github.com/nathancahill/Anycomplete)
+
+local obj = {}
+
+-- Options
+obj.engine = "google"
+
+-- Metadata
+obj.name = "Anycomplete"
+obj.version = "1.0"
+obj.author = "Nathan Cahill <nathan@nathancahill.com>"
+obj.homepage = "https://github.com/nathancahill/Anycomplete"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+local endpoints = {
+    google = "https://suggestqueries.google.com/complete/search?client=firefox&q=%s",
+    duckduckgo = "https://duckduckgo.com/ac/?q=%s",
+}
+
+-- Anycomplete
+function obj:anycomplete()
+    local current = hs.application.frontmostApplication()
+    local tab = nil
+    local copy = nil
+    local choices = {}
+
+    local chooser = hs.chooser.new(function(choosen)
+        if copy then copy:delete() end
+        if tab then tab:delete() end
+        current:activate()
+
+        if not choosen then return end
+        hs.eventtap.keyStrokes(choosen.text)
+    end)
+
+    -- Removes all items in list
+    function reset()
+        chooser:choices({})
+    end
+
+    tab = hs.hotkey.bind('', 'tab', function()
+        local id = chooser:selectedRow()
+        local item = choices[id]
+        -- If no row is selected, but tab was pressed
+        if not item then return end
+        chooser:query(item.text)
+        reset()
+        updateChooser()
+    end)
+
+    copy = hs.hotkey.bind('cmd', 'c', function()
+        local id = chooser:selectedRow()
+        local item = choices[id]
+        if item then
+            chooser:hide()
+            hs.pasteboard.setContents(item.text)
+            hs.alert.show("Copied to clipboard", 1)
+        else
+            hs.alert.show("No search result to copy", 1)
+        end
+    end)
+
+    function updateChooser()
+        local string = chooser:query()
+        local query = hs.http.encodeForQuery(string)
+        -- Reset list when no query is given
+        if string:len() == 0 then return reset() end
+
+        hs.http.asyncGet(string.format(endpoints[self.engine], query), nil, function(status, data)
+            if not data then return end
+
+            local ok, results = pcall(function() return hs.json.decode(data) end)
+            if not ok then return end
+
+            if self.engine == "google" then
+                choices = hs.fnutils.imap(results[2], function(result)
+                    return {
+                        ["text"] = result,
+                    }
+                end)
+            elseif self.engine == "duckduckgo" then
+                choices = hs.fnutils.imap(results, function(result)
+                    return {
+                        ["text"] = result["phrase"],
+                    }
+                end)
+            end
+
+            chooser:choices(choices)
+        end)
+    end
+
+    chooser:queryChangedCallback(updateChooser)
+    chooser:searchSubText(false)
+    chooser:show()
+end
+
+function obj:bindHotkeys(mapping)
+    mapping = mapping or {{"cmd", "alt", "ctrl"}, "g"}
+    hs.hotkey.bind(mapping[1], mapping[2], function() obj:anycomplete() end)
+end
+
+return obj
